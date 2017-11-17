4f7b820ddb5e06024f5c314c57c1e3c5acfb02ae
berangerelatouche <berangerelatouche@fr.ibm.com>
2017-11-17 10:27:36 +0100

    [INIT] project

---
+++ initiation-swift-tp/initiation-swift-tp/MainViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MainViewController.swift
+//  initiation-swift-tp
+//
+//  Created by Bérangère La Touche on 17/11/2017.
+//  Copyright © 2017 Bérangère La Touche. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = "Liste d'élèves"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
